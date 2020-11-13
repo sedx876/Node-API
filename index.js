00e703ba9ebb99f77d98eac2c34959c4c543e7db
sedx876 <swatkins876@gmail.com>
2020-11-13 05:08:51 -0500

    code cleanup

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import express from 'express'
-import bodyParser from 'body-parser'
+import express from "express";
+import bodyParser from "body-parser";
 
-const app = express()
-const PORT = 5000
+import usersRoutes from "./routes/users.js";
 
-app.use(bodyParser.json())
+const app = express();
+const PORT = 5000;
 
-app.get('/', (req, res) => {
-  console.log('[TEST]')
-  res.send('Hello from TEST')
-})
+app.use(bodyParser.json());
 
-app.listen(PORT, () => 
-console.log(`Server running on port: http://localhost:${PORT}`))
+app.use("/people", usersRoutes);
+app.get("/", (req, res) => res.send("Welcome to the Users API!"));
+app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
+
+app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
