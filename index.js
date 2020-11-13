dd6f11af7197b321ec1115a33da768228334932d
sedx876 <swatkins876@gmail.com>
2020-11-13 05:04:02 -0500

    added router/users.js

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ import express from 'express'
 import bodyParser from 'body-parser'
 
 const app = express()
+const PORT = 5000
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  console.log('[TEST]')
+  res.send('Hello from TEST')
+})
+
+app.listen(PORT, () => 
+console.log(`Server running on port: http://localhost:${PORT}`))
